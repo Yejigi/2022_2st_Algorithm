62b45a966e5fa40f3e34654e779dde5be4b8beb0
parkjonghyeok2 <parkjonghyeok2000@gmail.com>
2022-05-10 23:27:23 +0900

    parkjonghyeok
    5/10

---
+++ Week1/7팀/bj1001_pjh.java
@@ -0,0 +1,13 @@
+package bj_;
+import java.util.Scanner;
+
+public class bj_1000 {
+	public static void main(String[] args) {
+	Scanner ex = new Scanner(System.in);
+	int a = ex.nextInt();
+	int b= ex.nextInt();
+	
+	System.out.println(a-b);
+	}
+
+}
