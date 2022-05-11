97a32f39eed713278222db9d95004e3ffc20d8ed
Mingyu Kim <95063288+Tureve@users.noreply.github.com>
2022-05-11 20:24:25 +0900

    Create bj2475_kmg.java

---
+++ Week1/3팀/bj2475_kmg.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class bj2475_kmg {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+
+        for(int i=0; i<5; i++) {
+            int input = sc.nextInt();
+            sum += input*input;
+        }
+
+        System.out.println(sum%10);
+    }
+}
