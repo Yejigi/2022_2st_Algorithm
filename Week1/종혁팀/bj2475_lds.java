c4b9588fe46c7793f1dacaa6f9741241c1c8edb5
DaSom253 <idasom253@gmail.com>
2022-05-13 02:06:14 +0900

    lds week1
    ++수정

---
+++ Week1/종혁팀/bj2475_lds.java
@@ -0,0 +1,18 @@
+package Week1.7팀;
+
+import java.util.Scanner;
+
+public class bj2475_lds {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = 5;
+		int anwer = 0;
+		for (int i = 0; i < n; i++) {
+			int a = in.nextInt();
+			anwer += a*a;
+		}
+		
+		System.out.println(anwer%10);
+	}
+}
