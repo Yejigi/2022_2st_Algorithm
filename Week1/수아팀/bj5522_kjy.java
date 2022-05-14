989e9b295d5db826ecb19f89d97e725ed5b2cbe6
suah03 <1128smil@gmail.com>
2022-05-15 03:38:15 +0900

    Update bj5522_kjy.java

--- Week1/수아팀/bj5522_kjy.java
+++ Week1/수아팀/bj5522_kjy.java
@@ -10,6 +10,6 @@ public class bj5522_kjy_java {
         for (int i = 0; 5 > i; i++) {
             sum += in.nextInt();
         }
-        System.out.println(sum);
+        System.out.println(sum); //수아: 2475문제와 비슷한듯..다른...좋네요~˙˚ʚ(´◡`)ɞ˚˙
     }
 }
