c906aecdf2ca33540936d33f10761863bdd60c26
jinminseok <jinkss8077@naver.com>
2022-05-15 18:00:08 +0900

    Week1

---
+++ Week1/수빈팀/bj1550_jms.java
@@ -0,0 +1,13 @@
+import java.util.*;
+ 
+class Main {
+  public static void main(String[] args) throws Exception {
+      Scanner sc=new Scanner(System.in);
+      String a=sc.next();
+      System.out.println(Integer.parseInt(a,16));
+     
+      }
+}
+
+
+출처: https://blue-boy.tistory.com/75 [코딩배우는 학생🌎]
