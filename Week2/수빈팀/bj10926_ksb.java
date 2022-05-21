0acbbec2e923a966ff3c4450b120915020cb47ef
Kim-Su-B <hbsubin1@naver.com>
2022-05-21 15:54:23 +0900

    bj10926_ksb

---
+++ Week2/수빈팀/bj10926_ksb.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// ??! 10926번
+		Scanner in = new Scanner(System.in);
+		
+		String id = in.next();
+		
+		System.out.println(id + "??!");
+
+	}
+
+}
